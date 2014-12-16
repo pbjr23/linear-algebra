5cff661e45877de21eadf90b5efe289cd3d0508f
Adel Qalieh <aqalieh95@gmail.com>
2014-12-15 22:34:10 -0500

    Refactor main file to use optparse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import sys
-import readline
-import rlcompleter
-import code
+import optparse
 
 
 def print_helptext():
@@ -12,22 +10,29 @@ def print_helptext():
       python main.py app    Open web interface
 
     Options:
-      -h --help    Show this screen
-      --version    Show version
+      -h --help     Show this screen
+      -d --debug    Start webapp in debug mode
+      --version     Show version
     """
 
+parser = optparse.OptionParser(add_help_option=False)
+parser.add_option('-h', '--help', action="store_true")
+parser.add_option('--version', action="store_true")
+parser.add_option('-d', '--debug', action="store_true")
+options, args = parser.parse_args()
+
 try:
-    if "-h" in sys.argv or "--help" in sys.argv:
-        print_helptext()
-    elif sys.argv[1] == "--version":
+    if options.version:
         print "Python Linear Algebra Toolkit 1.0.0"
     elif sys.argv[1] == "cli":
         import cli
     elif sys.argv[1] == "app":
         from app.app import app
-        app.run()
+        app.run(debug=options.debug)
+    elif options.help:
+        print_helptext()
     else:
-        print "Unknown command: " + sys.argv[1]
+        print "Unknown command: " + " ".join(sys.argv[1:])
         print_helptext()
 except:
     print_helptext()
