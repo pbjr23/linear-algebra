7944ad70fe3ad91f12358fa2f4e9aa032f6b42ef
Adel Qalieh <aqalieh95@gmail.com>
2014-12-15 23:29:56 -0500

    Add contact form

--- app/static/js/agency.js
+++ app/static/js/agency.js
@@ -24,3 +24,39 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$(document).ready(function(){
+  $('#contact-submit').click(function(evt) {
+    evt.preventDefault();
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var comments = $('#message').val();
+    if ((name !== "") && (email!=="") && (comments!=="")) {
+    $.ajax({
+      url: "https://docs.google.com/forms/d/1XXQWJu-skbOjjehRrDPJ8dAOyGxQv5lImpTi7_kaKHQ/formResponse",
+        type: "POST",
+        dataType: "xml",
+        data: {
+          'entry.1445318317' : name,
+          'entry.938466652' : email,
+          'entry.1687927010' : comments,
+          'draftResponse': [,,"-441002587461861719"],
+          'pageHistory': 0,
+          'fbzx' : -441002587461861719
+        },
+        statusCode: {
+          0: function() {
+            $('#contactForm').prepend('<p>Thanks for your feedback - we will get back to you soon!</p>');
+            $('#contactForm input, #contactForm textarea').val("");
+          },
+          200: function(){
+            $('#contactForm').prepend('<p>Thanks for your feedback - we will get back to you soon!</p>');
+            $('#contactForm input, #contactForm textarea').val("");
+          }
+       }
+    });
+    } else {
+      $('#contactForm').prepend('<p>Form not complete - please try again.</p>');
+    }
+  });
+});
